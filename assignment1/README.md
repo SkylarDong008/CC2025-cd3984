c2413961506f07a3ef2625d58a0a36367d8d9336
Chuyao Dong(Skylar) <cd3984@nyu.edu>
2025-09-19 14:49:23 -0400

    update

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,4 +1,4 @@
-# Assignment 
+# Assignment1
 
 Image with url:  
 ![image description](https://github.com/SkylarDong008/CC2025-cd3984/blob/4384481abcd3a3df3072f77bbb0869a4b9719770/assignment1/Suprematist%20Composition.jpg)
--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -27,6 +27,7 @@ function drawStrips(stripeWidth){
 }
 
 function draw() {
+  // set the stripeWidth to 20
   drawStrips(20);
   
   // adding shadows for each shape
@@ -40,12 +41,16 @@ function draw() {
   fill(avocadoGreen);
   triangle(width*0.24, height*0.16, width*0.3, height*0.8, width*0.88, height*0.48);
 
-  // divide shapes into different groups 
-  // using push() and pop() to control the rotate angle of this group
+  // Divide shapes into different groups 
+  // Group 1: consists 4 rectangles in the top left of this painting
+  // using push() and pop() to control the same rotate angle of this group
   push();
-  fill(pastelPink);
+  // Treat these rectangles as a cohesive group.
+  // Set rectMode(CENTER) so that the (x, y) coordinates are the rectangle centers
   rectMode(CENTER);
+  // rotate 4 rectangulars in the same degree
   rotate(-PI/6.5)
+  fill(pastelPink);
   rect(width*0.1, height*0.4, width*0.38, height*0.08);
 
   fill(warmBrown);
@@ -58,9 +63,8 @@ function draw() {
   rect(width*0.34, height*0.428, width*0.1, height*0.012);
   pop();
 
-  
-
-
+  // Create the top left square in softBeige
+  // Rotate it slightly more than the Group 1 rectangles
   push();
   fill(softBeige);
   rectMode(CENTER);
@@ -68,7 +72,8 @@ function draw() {
   rect(width*0.05, height*0.32, height*0.08, height*0.08);
   pop();
 
-  //右上方斜着
+  // Goup 2: consists of 3 rectangulars in the top right of this painting
+  // Using the same strategy in Group 1
   push();
   fill(softBeige);
   rectMode(CENTER);
@@ -82,15 +87,15 @@ function draw() {
   rect(width*0.001, height*0.69, width*0.35, height*0.025);
   pop();
 
-  //右下的圆
+  // Create the bottom right circle in pastelPink
   fill(pastelPink);
   circle(width*0.78, height*0.63, 130);
 
-  //左下的正方形
+  // Create the bottom left square in avocadoGreen
   fill(avocadoGreen);
   square(width*0.1, height*0.88, 25);
 
-  //右下斜着的2个长条
+  // Group 3: consists of 2 rectangles at the bottom right 
   push();
   fill(pastelPink);
   rectMode(CENTER);
@@ -100,7 +105,7 @@ function draw() {
   rect(width*0.99, height*0.48, width*0.15, height*0.02);
   pop();
 
-  // 
+  // Group 4: consists of 3 rectangles at the bottom left of this painting
   push();
   rotate(PI/6);
   translate(width*0.34, height*0.17);
@@ -114,6 +119,3 @@ function draw() {
   rect(width*0.22, height*0.26, width*0.08, height*0.04);
 
 }
-
-
-
